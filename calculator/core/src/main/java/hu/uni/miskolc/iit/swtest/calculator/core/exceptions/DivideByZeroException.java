e847451233e8edce049076e3b6f71f5871edcbbc
Zsolt Toth <tothzs@iit.uni-miskolc.hu>
2018-10-01 17:38:36 +0200

    core module of the calculator has been implemented. Calculator.java service has been defined.

---
+++ calculator/core/src/main/java/hu/uni/miskolc/iit/swtest/calculator/core/exceptions/DivideByZeroException.java
@@ -0,0 +1,22 @@
+package hu.uni.miskolc.iit.swtest.calculator.core.exceptions;
+
+public class DivideByZeroException extends Exception {
+    public DivideByZeroException() {
+    }
+
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+
+    public DivideByZeroException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DivideByZeroException(Throwable cause) {
+        super(cause);
+    }
+
+    public DivideByZeroException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
